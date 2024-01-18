c991535b614baded7966194683990c9921277d84
unknown <mfmcarlos.student@ua.edu.ph>
2024-01-18 11:55:43 +0800

    first commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+      <p><b>Hello! I am Miguel Francis Carlos a student web developer!!</b></p>
+        <p><b>10 Things That Require Zero Talent</b></p>
+        <ul>
+        <li>1. Being on Time</li>
+        <li>2. Making An Effort</li>
+        <li>3. Being High Energy</li>
+        <li>4. Having A Positive Attitude</li>
+        <li>5. Being Passionate</li>
+        <li>6. Using Good Body Language</li>
+        <li>7. Being Coachable</li>
+        <li>8. Doing A Little Extra</li>
+        <li>9. Being Prepared</li>
+        <li>10. Having A Strong Work Ethic</li>
+        </ul>
+        <p><b>3 Things to expect to this subject</b></p>
+        <li>1. Hard</li>
+        <li>2. Enjoyable</li>
+        <li>3. A lot of work and thinking</li>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
